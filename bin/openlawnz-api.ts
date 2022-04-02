980685240fcb4c05ca35fc153473e4e6972433dc
William Parry <williamparry@gmail.com>
2022-04-03 08:56:16 +1000

    Separate dev/prod stacks. Update deps.

--- bin/openlawnz-api.ts
+++ bin/openlawnz-api.ts
@@ -3,9 +3,36 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { OpenlawnzApiStack } from '../lib/openlawnz-api-stack';
 
+require('dotenv').config()
+
+const DEPLOYMENT_KEY: string = process.env.DEPLOYMENT_KEY!
+
+if (
+	!process.env.DEPLOYMENT_MAP ||
+	!process.env.DEPLOYMENT_KEY
+) {
+	throw new Error("Missing required environment variables");
+}
+
+const DEPLOYMENT_MAP: {
+	[index: string]: {
+		environment: string,
+		domainName: string
+	}
+} = JSON.parse(process.env.DEPLOYMENT_MAP!);
+
+
+const toDeploy = DEPLOYMENT_MAP[DEPLOYMENT_KEY];
+
+if (!toDeploy) {
+	throw new Error(`No matching deployment. Passed in key '${DEPLOYMENT_KEY}'`)
+}
+
+const environmentToDeploy = toDeploy.environment;
+
 const app = new cdk.App();
 
-new OpenlawnzApiStack(app, 'OpenlawnzApiStack', {
+new OpenlawnzApiStack(app, 'OpenlawnzApiStack' + environmentToDeploy, {
 	env: {
 		account: process.env.CDK_DEFAULT_ACCOUNT,
 		region: process.env.CDK_DEFAULT_REGION
